563438574b5778b7fbb50aa3e75567c901538596
Sam Ageloff <sageloff@hugeinc.com>
2015-09-05 19:38:42 -0400

    Routing

---
+++ client/app/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+angular.module('ambassadorApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router',
+  'xeditable'
+])
+
+.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+  $stateProvider
+    .state('main', {
+      url: '/',
+      templateUrl: 'app/main/main.html',
+      controller: 'MainCtrl'
+    })
+    .state('referral', {
+      url: '/:id',
+      templateUrl: 'app/referral/referral.html',
+      controller: 'ReferralCtrl'
+    })
+    .state('landing', {
+      url: '/landing/*path',
+      templateUrl: 'app/landing/landing.html',
+      controller: 'LandingCtrl',
+      reloadOnSearch: false
+    })
+
+  $urlRouterProvider
+    .otherwise('/');
+
+  $locationProvider.html5Mode(true);
+})
+
+.run(function(editableOptions) {
+  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
